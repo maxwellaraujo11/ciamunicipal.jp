5e79312d884de88372e7c00b08dd7b4d803112e1
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-04-29 23:15:39 -0300

    implementei java script para mais animações

---
+++ script.js
@@ -0,0 +1,15 @@
+const myObserver = new IntersectionObserver( (entries) => {
+    entries.forEach( (entry) => {
+      if(entry.isIntersecting){
+        entry.target.classList.add('show')
+      } else {
+        entry.target.classList.remove('show')
+      }
+    })
+})
+
+const slideleft = document.querySelectorAll(".slider-left")
+const slideright = document.querySelectorAll(".slider-right")
+
+slideleft.forEach((element) => myObserver.observe(element))
+slideright.forEach((element) => myObserver.observe(element))
