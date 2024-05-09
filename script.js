f19c684e8fbea4fea746f128407ad919620081c9
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2024-05-09 14:53:40 -0300

    funcionalidade que abre apenas uma tag detais por vez

--- script.js
+++ script.js
@@ -15,3 +15,19 @@ slideleft.forEach((element) => myObserver.observe(element))
 slideright.forEach((element) => myObserver.observe(element))
 
 
+
+// Obtém todos os elementos <details>
+var detailsElements = document.querySelectorAll('details');
+
+// Adiciona um evento de clique a cada elemento <summary>
+detailsElements.forEach(function(details) {
+    var summary = details.querySelector('summary');
+    summary.addEventListener('click', function() {
+        // Fecha todos os detalhes, exceto o que foi clicado
+        detailsElements.forEach(function(otherDetails) {
+            if (otherDetails !== details) {
+                otherDetails.removeAttribute('open');
+            }
+        });
+    });
+});
