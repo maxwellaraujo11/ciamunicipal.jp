72e89a93edf1b8ec9877180a0b53e0f90bf295d0
Maxwell Araujo <maxwellaraujo11@hotmail.com>
2025-01-25 03:33:17 -0300

    retirei um butão para ajustes

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-const myObserver = new IntersectionObserver( (entries) => {
-    entries.forEach( (entry) => {
-      if(entry.isIntersecting){
-        entry.target.classList.add('show')
-      } else {
-        entry.target.classList.remove('show')
-      }
-    })
-})
+const myObserver = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      entry.target.classList.add("show");
+    } else {
+      entry.target.classList.remove("show");
+    }
+  });
+});
 
-const slideleft = document.querySelectorAll(".slider-left")
-const slideright = document.querySelectorAll(".slider-right")
+const slideleft = document.querySelectorAll(".slider-left");
+const slideright = document.querySelectorAll(".slider-right");
 
-slideleft.forEach((element) => myObserver.observe(element))
-slideright.forEach((element) => myObserver.observe(element))
-
-
+slideleft.forEach((element) => myObserver.observe(element));
+slideright.forEach((element) => myObserver.observe(element));
 
 // Obtém todos os elementos <details>
-var detailsElements = document.querySelectorAll('details');
+var detailsElements = document.querySelectorAll("details");
 
 // Adiciona um evento de clique a cada elemento <summary>
-detailsElements.forEach(function(details) {
-    var summary = details.querySelector('summary');
-    summary.addEventListener('click', function() {
-        // Fecha todos os detalhes, exceto o que foi clicado
-        detailsElements.forEach(function(otherDetails) {
-            if (otherDetails !== details) {
-                otherDetails.removeAttribute('open');
-            }
-        });
+detailsElements.forEach(function (details) {
+  var summary = details.querySelector("summary");
+  summary.addEventListener("click", function () {
+    // Fecha todos os detalhes, exceto o que foi clicado
+    detailsElements.forEach(function (otherDetails) {
+      if (otherDetails !== details) {
+        otherDetails.removeAttribute("open");
+      }
     });
+  });
 });
